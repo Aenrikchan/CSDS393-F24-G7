1156f7cc8a0b41178177ec4dbebae074b7047bdf
rick666666 <rickning951@gmail.com>
2024-11-22 12:36:09 -0500

    add clickable URL for Related News article
    Update HTML,CSS,Javascript
    
    two new function in Javascript
    toggleDivVisibility: allow to use divID to hide/display a div block in HTML using javascript function.
    
    updateURL: update the Related news article by allowing a list to pass into the function and update it in html to become clickable link.

--- popup.js
+++ popup.js
@@ -1,4 +1,44 @@
 // popup.js
+//this function is use to hide/displlay a block of div in html(intended to use for Alternative news link)
+function toggleDivVisibility(divId) {
+  const div = document.getElementById(divId);
+  if (div.style.display === "none") {
+      div.style.display = "block"; // Show the div
+  } else {
+      div.style.display = "none"; // Hide the div
+  }
+}
+
+//input a list of url and it will modify the html to show clickable link for each url. 
+function updateURL(urls) {
+  const urlList = document.getElementById('urlList'); // Select the <ul> element
+  urlList.innerHTML = ''; // Clear existing content
+
+  // Loop through the URLs array and create list items
+  urls.forEach(url => {
+      const listItem = document.createElement('li'); // Create a new <li>
+      const link = document.createElement('a'); // Create a new <a> tag
+
+      link.href = url; // Set the URL
+      link.target = '_blank'; // Open in a new tab
+      link.rel = 'noopener noreferrer'; // Add security attributes
+      link.textContent = url; // Display the URL as the link text
+
+      listItem.appendChild(link); // Append the <a> tag to the <li>
+      urlList.appendChild(listItem); // Append the <li> to the <ul>
+  });
+}
+
+// Example Usage of p
+const urls = [
+  'https://www.npr.org/',
+  'https://www.bbc.com/news',
+  'https://www.cnn.com'
+];
+updateURL(urls);
+
+
+
 document.getElementById('analyzeButton').addEventListener('click', () => {
   // get current tab
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
